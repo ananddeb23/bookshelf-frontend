86df2eec5431d7b28801d9c2a6a8fdd321c13a9b
anand <ananddeb23@gmail.com>
2018-02-21 12:59:07 +0530

    like status is updated dynamically

--- src/Components/ContentBody/ContentCase/Card/Card.js
+++ src/Components/ContentBody/ContentCase/Card/Card.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
+// import { connect } from 'react-redux';
+// import { updateLike } from '../redux/actions';
 
 class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likeStatus: this.props.value.likeStatus,
+      bookid: this.props.value.bookid,
+      bookname: this.props.value.bookname,
+      rating: this.props.value.rating,
+      author: this.props.value.author,
+    };
+    this.handleupdate = this.handleupdate.bind(this);
+  }
+  handleupdate() {
+    const url = '/togglelike/';
+    const urltoreq2 = url + this.state.bookid;
+    fetch(urltoreq2)
+      .then((responserec) => {
+        responserec.text().then((records) => {
+          console.log((records));
+          // const recordsparsed = JSON.parse(records);
+          this.setState({ likeStatus: records });
+        });
+      });
+  }
   render() {
     return (<div className="Card-outer">
       <img src="https://images-na.ssl-images-amazon.com/images/I/51VNlzbfpXL._SX331_BO1,204,203,200_.jpg" width="120px" height="40px" />
-      <h4>{this.props.value.likeStatus}</h4>
-      <h4>{this.props.value.bookname}</h4>
-      <h4>{this.props.value.rating}</h4>
-      <h4>{this.props.value.author}</h4>
+      <h4 onClick={this.handleupdate}>{this.state.likeStatus}</h4>
+      <h4>{this.state.bookid}</h4>
+      <h4>{this.state.bookname}</h4>
+      <h4>{this.state.rating}</h4>
+      <h4>{this.state.author}</h4>
 
-            </div>);
+    </div>);
   }
 }
 Card.defaultProps = {
 };
 Card.propTypes = {
 };
-export default Card;
+
+// const mapDispatchToProps = dispatch => ({
+//   updateLike: bookid => dispatch(updateLike(bookid)),
+
+// });
+
+export default (Card);
+
